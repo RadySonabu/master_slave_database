b7a14c1fd1b3f2b646fff63e64f5ef5754d2bf08
RadySonabu <ardynubanos@yahoo.com>
2020-05-14 20:48:09 +0800

    Fixed FK that were automatically generated

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.validators import RegexValidator
 import re
 
+# individual contact
+
 
 class Contact_A00(models.Model):
 
@@ -54,11 +56,13 @@ class Contact_A00(models.Model):
                     self.contact_id = 'CON' + zero_filled_number
         return super(Contact_A00, self).save(*args, **kwargs)
 
+# individual contact skill and comments
+
 
 class Contact_A01(models.Model):
 
     contact_a01_rec = models.AutoField(primary_key=True)
-    contact_id = models.CharField(max_length=50, default='CON0000000000')
+    contact_id = models.ForeignKey(Contact_A00, on_delete=models.DO_NOTHING)
     skill_id = models.IntegerField()
     comments = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True, null=True)
@@ -67,37 +71,14 @@ class Contact_A01(models.Model):
     def __str__(self):
         return f'{self.contact_id} {self.skill_id} {self.comments}'
 
-    def save(self, *args, **kwargs):
-        if self._state.adding:
 
-            last_id_raw = Contact_A01.objects.all().aggregate(
-                largest=models.Max('contact_id'))['largest']
-            if last_id_raw == None:
-                last_id_raw = 'CON0000000000'
-            else:
-                last_id = re.sub('[^0-9]', '', last_id_raw)
-                print(type(last_id))
-                if last_id is not None:
-                    if last_id == 0:
-                        last_id = 0
-                        leading_zero = int(last_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-                    else:
-                        leading_zero = int(last_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-
-                    self.contact_id = 'CON' + zero_filled_number
-        return super(Contact_A01, self).save(*args, **kwargs)
+# individual contacts endorsement
 
 
 class Contact_A02(models.Model):
 
     contact_a02_rec = models.AutoField(primary_key=True)
-    contact_id = models.CharField(max_length=50, default='CON0000000000')
+    contact_id = models.ForeignKey(Contact_A00, on_delete=models.DO_NOTHING)
     endorsement_id = models.IntegerField()
     message = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True, null=True)
@@ -106,31 +87,8 @@ class Contact_A02(models.Model):
     def __str__(self):
         return f'{self.contact_id} {self.endorsement_id} {self.message}'
 
-    def save(self, *args, **kwargs):
-        if self._state.adding:
 
-            last_id_raw = Contact_A02.objects.all().aggregate(
-                largest=models.Max('contact_id'))['largest']
-            if last_id_raw == None:
-                last_id_raw = 'CON0000000000'
-            else:
-                last_id = re.sub('[^0-9]', '', last_id_raw)
-                print(type(last_id))
-                if last_id is not None:
-                    if last_id == 0:
-                        last_id = 0
-                        leading_zero = int(last_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-                    else:
-                        leading_zero = int(last_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-
-                    self.contact_id = 'CON' + zero_filled_number
-        return super(Contact_A02, self).save(*args, **kwargs)
+# group contact
 
 
 class Group_A00(models.Model):
@@ -183,11 +141,13 @@ class Group_A00(models.Model):
                     self.group_id = 'GRP' + zero_filled_number
         return super(Group_A00, self).save(*args, **kwargs)
 
+# group contact with role
+
 
 class Group_A01(models.Model):
     group_a01_rec = models.AutoField(primary_key=True)
-    group_id = models.CharField(max_length=50, default='GRP0000000000')
-    contact_id = models.CharField(max_length=50, default='CON0000000000')
+    group_id = models.ForeignKey(Group_A00, on_delete=models.CASCADE)
+    contact_id = models.ForeignKey(Contact_A00, on_delete=models.CASCADE)
     group_role = models.CharField(max_length=50)
     comments = models.TextField()
     status = models.BooleanField(default=True)
@@ -195,51 +155,3 @@ class Group_A01(models.Model):
 
     def __str__(self):
         return f'{self.group_a01_rec} {self.group_id} {self.contact_id} {self.group_role} {self.comments}'
-
-    def save(self, *args, **kwargs):
-        if self._state.adding:
-
-            group_id_raw = Group_A01.objects.all().aggregate(
-                largest=models.Max('group_id'))['largest']
-            contact_id_raw = Group_A01.objects.all().aggregate(
-                largest=models.Max('group_id'))['largest']
-            if group_id_raw == None:
-                group_id_raw = 'GRP0000000000'
-            else:
-                group_id = re.sub('[^0-9]', '', group_id_raw)
-                print(type(group_id))
-                if group_id is not None:
-                    if group_id == 0:
-                        group_id = 1
-                        leading_zero = int(group_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-                    else:
-                        leading_zero = int(group_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-
-                    self.group_id = 'GRP' + zero_filled_number
-
-            if contact_id_raw == None:
-                contact_id_raw = 'GRP0000000000'
-            else:
-                contact_id = re.sub('[^0-9]', '', contact_id_raw)
-                print(type(contact_id))
-                if contact_id is not None:
-                    if contact_id == 0:
-                        contact_id = 1
-                        leading_zero = int(contact_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-                    else:
-                        leading_zero = int(contact_id) + 1
-                        number_str = str(leading_zero)
-                        zero_filled_number = number_str.zfill(10)
-                        print(type(zero_filled_number))
-
-                    self.contact_id = 'CON' + zero_filled_number
-        return super(Group_A01, self).save(*args, **kwargs)
